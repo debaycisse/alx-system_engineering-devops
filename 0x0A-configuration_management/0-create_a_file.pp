0b670e27042f97290c593d31485455d3323479bb
Azeez Adebayo <debaycisse@gmail.com>
2024-04-19 21:18:40 +0100

    update code to use path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file{'/tmp/school':
+  path    => '/tmp/school',
   ensure  => present,
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
 }
