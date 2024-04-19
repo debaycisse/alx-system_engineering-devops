7377047517a05eccbd0a7c4851c2da4c6fcfe7f5
Azeez Adebayo <debaycisse@gmail.com>
2024-04-19 21:25:01 +0100

    update ensure value to file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 file{'/tmp/school':
   path    => '/tmp/school',
-  ensure  => present,
+  ensure  => file,
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+
 }
