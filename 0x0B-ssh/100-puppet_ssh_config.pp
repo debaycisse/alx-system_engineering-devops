bbdd863df33999934cdfd32414584029ddd11247
Azeez Adebayo <debaycisse@gmail.com>
2024-04-21 13:15:42 -0700

    change ensure's value to present

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # creates a configuration file for an ssh connection
 file {'/root/.ssh/config':
-  ensure  => file,
+  ensure  => present,
   content => "Host 530748-web-01 34.229.55.139\n\tHostName 34.229.55.139\n\tIdentitiesOnly = yes\n\tIdentityFile ~/.ssh/school",
   mode    => '0644',
 }
