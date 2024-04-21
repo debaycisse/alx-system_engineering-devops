2576e4924e7b81b2152f23407f6c8d36f5f3d8f6
Azeez Adebayo <debaycisse@gmail.com>
2024-04-21 12:35:14 -0700

    work in progress

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+# creates a configuration file for an ssh connection
+file {'~/.ssh/config'
+  ensure  => file,
+  content => "Host 530748-web-01 34.229.55.139\n\tHostName 34.229.55.139\n\tIdentitiesOnly = yes\n\tIdentityFile ~/.ssh/school"
+  mode    => "0600"
+}
