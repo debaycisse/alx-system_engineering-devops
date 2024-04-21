2ab167c94459fba5f27e5b504db0127ec7e4d072
Azeez Adebayo <debaycisse@gmail.com>
2024-04-21 13:12:27 -0700

    ensure puppet-lint compliance

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,5 +2,5 @@
 file {'/root/.ssh/config':
   ensure  => file,
   content => "Host 530748-web-01 34.229.55.139\n\tHostName 34.229.55.139\n\tIdentitiesOnly = yes\n\tIdentityFile ~/.ssh/school",
-  mode    => "0644",
+  mode    => '0644',
 }
