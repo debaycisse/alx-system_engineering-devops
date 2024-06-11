b2c3fa6ca7c27ec8cd14e126c2f0994171129b4c
Azeez Adebayo <debaycisse@gmail.com>
2024-06-11 12:51:13 +0100

    enure pep8 compliance

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
-# script changes the value of nginx configuration of the ULIMIT from 15 to 500 so that each nginx service worker can have more files opened. This is required to enable nginx to serve multiple simultaneous requests.
-exec { "increases the value of the ULIMIT":
+# script changes the value of nginx configuration of the ULIMIT from 15 to 500.
+exec { 'increases the value of the ULIMIT':
     command => "sed -i 's/ULIMIT=.*/ULIMIT=\"-n 500\"/g' /etc/default/nginx",
-    path    => "/bin/"
+    path    => '/bin/'
 }
 
-exec { "restarts the nginx web server":
-    command => "service nginx restart",
-    path    => ["/usr/sbin/", "/bin/", "/usr/bin/"]
+exec { 'restarts the nginx web server':
+    command => 'service nginx restart',
+    path    => ['/usr/sbin/', '/bin/', '/usr/bin/']
 }
