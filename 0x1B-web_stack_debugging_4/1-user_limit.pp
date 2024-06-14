2e50b6854cc71bdf02a183a4a56a8079e927e7f6
Azeez Adebayo <debaycisse@gmail.com>
2024-06-14 06:54:35 +0100

    correct the regex to match aplhanumeric propperly

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
 # script updates the limit's value for a number of opened file for user holberton
 exec { 'increases the hard limit value for user - holberton':
-    command => 'sed -i \'s/holberton hard nofile 4/holberton hard nofile 500/g\' /etc/security/limits.conf',
+    command => 'sed -i \'s/holberton hard [A-Za-z0-9]*/holberton hard nofile 500/g\' /etc/security/limits.conf',
     path    => ['/bin/', '/usr/bin/']
 }
 
 exec { 'increases the soft limit value for user - holberton':
-    command => 'sed -i \'s/holberton soft nofile 4/holberton soft nofile 500/g\' /etc/security/limits.conf',
+    command => 'sed -i \'s/holberton soft [A-Za-z0-9]*/holberton soft nofile 500/g\' /etc/security/limits.conf',
     path    => ['/bin/', '/usr/bin/']
 }
